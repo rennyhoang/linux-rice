38cc1ed0747e2aa9e76978a568aa87bccb3a17ab
Renny <rennyghoang@gmail.com>
2021-09-15 22:23:45 -0500

    bruh

--- suckless/dwm/dwm.c
+++ suckless/dwm/dwm.c
@@ -745,7 +745,7 @@ drawbar(Monitor *m)
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
             		/* fix overflow when window name is bigger than window width */
-			int mid = (w - TEXTW(m->sel->name)) / 2;
+			int mid = (w - (int)TEXTW(m->sel->name)) / 2;
 			/* make sure name will not overlap on tags even when it is very long */
 			mid = mid >= lrpad / 2 ? mid : lrpad / 2;
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
